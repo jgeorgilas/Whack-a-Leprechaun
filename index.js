c1258a7751d6c5b073300985a48c2bcb2bc49c29
Your Name <justin.georgilas@gmail.com>
2023-03-25 14:30:04 -0700

    worked on pop up

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const pot = document.querySelectorAll('.pot1');
+const greenGuy = document.querySelectorAll('.leprechaun');
+const score = document.querySelector('#score');
+const timer = document.querySelector('#timer');
+const start = document.querySelector('#start');
+const end = document.querySelector('#end');
 
-
+let randnumber = Math.round(Math.random() * 10);
+if (randnumber === 1) {
+  let imageElement = document.getElementById("leprechaun-image");
+  imageElement.src = "./Assets/leprechaun.webp";
+  imageElement.style.display = "block";
+}
