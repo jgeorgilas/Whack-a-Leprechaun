9169fb332ab4e9d1b6165d4c0465abcae7cd53f6
Your Name <justin.georgilas@gmail.com>
2023-03-27 21:31:56 -0700

    polishing

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-const pot = document.querySelectorAll('.pot1');
-const greenGuy = document.querySelectorAll('.leprechaun');
-const score = document.querySelector('#score');
-const timer = document.querySelector('#timer');
-const start = document.querySelector('#start');
-const end = document.querySelector('#end');
+let timeUp = false;
+let score = 0;
+let timeLeft = 300;
+let elem = document.getElementById('countdown');
+let startButton = document.getElementById("start-button");
 
-let randnumber = Math.round(Math.random() * 10);
-if (randnumber === 1) {
-  let imageElement = document.getElementById("leprechaun-image");
-  imageElement.src = "./Assets/leprechaun.webp";
-  imageElement.style.display = "block";
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  let timerId = setInterval(countdown, 1000);
+  
+  function countdown() {
+    if (timeLeft == -1) {
+        clearInterval(timerId);
+        timesUp();
+    } else {
+        elem.innerHTML = timeLeft;
+        timeLeft--;
+    }
+  }
+
+  function timesUp() {
+    alert("Time's up!");
+  }
+
+  function showLeprechaun() {
+    let randnumber = Math.floor(Math.random() * 10) + 1;
+    console.log(randnumber);
+    let imageElement = document.getElementById("leprechaun-image" + randnumber);
+    imageElement.src = "./Assets/leprechaun.webp";
+    imageElement.style.display = "block";
+    
+    setTimeout(function() {
+      imageElement.style.display = "none";
+    }, 1000);
+    
+    imageElement.addEventListener("click", function() {
+      score++;
+      updateScore();
+      console.log("Clicked Him");
+    });
+  }
+
+  function updateScore() {
+    document.getElementById("score").textContent = score;
+  }
+
+  updateScore();
+  setInterval(showLeprechaun, 1500);
 }
